4aae98afb26f1edfe1c24274d687b38459d03076
vohoanglong0107 <mamlong34@gmail.com>
2023-03-31 08:18:49 +0700

    feat: rebuild bat cache when install
    GitOrigin-RevId: 70d373a8c9d246e50bc9749abed54537629ad96b

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ export -f install_config
 SRC_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SRC_DIR" || exit
 find . -type f -not -path "./install.sh" -exec sh -c 'install_config "$1"' shell {} \;
+
+bat cache --build
