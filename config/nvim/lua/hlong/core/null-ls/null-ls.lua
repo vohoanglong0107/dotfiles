2f58c6305870124754eb66284351213a809c43df
vohoanglong0107 <mamlong34@gmail.com>
2023-04-06 17:42:11 +0700

    ci: install husky and lint-staged
    With husky and lint-staged installed, null-ls format on save is not
    needed anymore, as prettier run on each file save is slow and
    unreliable
    
    GitOrigin-RevId: cbe9749cc51286e409355272dee076b0912505e8

--- config/nvim/lua/hlong/core/null-ls/null-ls.lua
+++ config/nvim/lua/hlong/core/null-ls/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	vim.notify(null_ls, vim.log.levels.WARN)
-	return
+  vim.notify(null_ls, vim.log.levels.WARN)
+  return
 end
 
 local utils = require("null-ls.utils").make_conditional_utils()
@@ -10,40 +10,27 @@ local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-	code_actions.eslint,
-	code_actions.gitrebase,
-	code_actions.gitsigns,
-	diagnostics.flake8,
-	diagnostics.golangci_lint,
-	diagnostics.hadolint,
-	diagnostics.shellcheck,
-	formatting.black.with({
-		extra_args = { "--fast" },
-	}),
-	formatting.prettier.with({
-		command = utils.root_has_file("node_modules/.bin/prettier") and "node_modules/.bin/prettier" or "prettier",
-		extra_filetypes = { "toml" },
-	}),
-	formatting.shfmt,
-	formatting.stylua,
+  code_actions.eslint,
+  code_actions.gitrebase,
+  code_actions.gitsigns,
+  diagnostics.flake8,
+  diagnostics.golangci_lint,
+  diagnostics.hadolint,
+  diagnostics.shellcheck,
+  formatting.black.with({
+    extra_args = { "--fast" },
+  }),
+  formatting.prettier.with({
+    command = utils.root_has_file("node_modules/.bin/prettier") and "node_modules/.bin/prettier" or "prettier",
+    extra_filetypes = { "toml" },
+  }),
+  formatting.shfmt,
+  formatting.stylua,
 }
 
-local null_ls_group = vim.api.nvim_create_augroup("null-ls", { clear = true })
-
 null_ls.setup({
-	border = "rounded",
-	debounce = 1000,
-	debug = false,
-	sources = sources,
-	on_attach = function(client)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "<buffer>",
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-				group = null_ls_group,
-			})
-		end
-	end,
+  border = "rounded",
+  debounce = 1000,
+  debug = false,
+  sources = sources,
 })
