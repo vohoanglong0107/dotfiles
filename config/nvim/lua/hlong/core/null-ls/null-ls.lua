0841b93b78016cb7faa2c5ffc392f6e8b8c1ca1b
vohoanglong0107 <mamlong34@gmail.com>
2023-04-09 14:28:04 +0700

    ci: lint python files through bazel
    GitOrigin-RevId: 767e4bd704acc22843140385c78094fe99e5f66f

--- config/nvim/lua/hlong/core/null-ls/null-ls.lua
+++ config/nvim/lua/hlong/core/null-ls/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  vim.notify(null_ls, vim.log.levels.WARN)
-  return
+	vim.notify(null_ls, vim.log.levels.WARN)
+	return
 end
 
 local utils = require("null-ls.utils").make_conditional_utils()
@@ -10,27 +10,28 @@ local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-  code_actions.eslint,
-  code_actions.gitrebase,
-  code_actions.gitsigns,
-  diagnostics.flake8,
-  diagnostics.golangci_lint,
-  diagnostics.hadolint,
-  diagnostics.shellcheck,
-  formatting.black.with({
-    extra_args = { "--fast" },
-  }),
-  formatting.prettier.with({
-    command = utils.root_has_file("node_modules/.bin/prettier") and "node_modules/.bin/prettier" or "prettier",
-    extra_filetypes = { "toml" },
-  }),
-  formatting.shfmt,
-  formatting.stylua,
+	code_actions.eslint,
+	code_actions.gitrebase,
+	code_actions.gitsigns,
+	diagnostics.flake8,
+	diagnostics.golangci_lint,
+	diagnostics.hadolint,
+	diagnostics.shellcheck,
+	formatting.isort,
+	formatting.black.with({
+		extra_args = { "--fast" },
+	}),
+	formatting.prettier.with({
+		command = utils.root_has_file("node_modules/.bin/prettier") and "node_modules/.bin/prettier" or "prettier",
+		extra_filetypes = { "toml" },
+	}),
+	formatting.shfmt,
+	formatting.stylua,
 }
 
 null_ls.setup({
-  border = "rounded",
-  debounce = 1000,
-  debug = false,
-  sources = sources,
+	border = "rounded",
+	debounce = 1000,
+	debug = false,
+	sources = sources,
 })
