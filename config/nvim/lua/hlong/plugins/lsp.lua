27d29d9d30ec4b3a1e4f1dd970e10b2da9ae595f
vohoanglong0107 <mamlong34@gmail.com>
2023-05-21 08:31:28 +0700

    chore: upgrade nerd fonts to v3
    GitOrigin-RevId: c918f110b569c2e973550ba547d7afbdb6950f67

--- config/nvim/lua/hlong/plugins/lsp.lua
+++ config/nvim/lua/hlong/plugins/lsp.lua
@@ -269,7 +269,7 @@ local disable_formatting = {
 local signs = {
 	{ name = "DiagnosticSignError", text = "" },
 	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
+	{ name = "DiagnosticSignHint", text = "󰌵" },
 	{ name = "DiagnosticSignInfo", text = "" },
 }
 
