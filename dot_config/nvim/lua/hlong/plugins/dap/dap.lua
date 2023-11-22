0994e23d9954fbfcfe4e9873559b89d5d41e8cb7
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-11-22 22:24:19 +0900

    feat(nvim): add keymaps for dap

--- dot_config/nvim/lua/hlong/plugins/dap/dap.lua
+++ dot_config/nvim/lua/hlong/plugins/dap/dap.lua
@@ -1,4 +1,43 @@
 return {
 	"mfussenegger/nvim-dap",
 	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>dc",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Start/Continue debugging",
+		},
+		{
+			"<leader>ds",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Step over",
+		},
+		{
+			"<leader>di",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Step in",
+		},
+		{
+			"<leader>do",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Step out",
+		},
+		{
+			"<leader>db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Toggle Breakpoint",
+		},
+	},
+	config = function()
+	end,
 }
