61c489fdb11b7112cd5cdcbc6d38a7ab13182833
vohoanglong0107 <vo.hoang.long@outlook.com>
2023-11-22 22:24:37 +0900

    feat(nvim): add icons to nvim dap

--- dot_config/nvim/lua/hlong/plugins/dap/dap.lua
+++ dot_config/nvim/lua/hlong/plugins/dap/dap.lua
@@ -39,5 +39,7 @@ return {
 		},
 	},
 	config = function()
+		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
 	end,
 }
