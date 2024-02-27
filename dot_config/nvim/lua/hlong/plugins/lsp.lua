3310676e095267b0c0277ff68eec0e007f95c1ab
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-02-27 11:17:39 +0900

    refactor: using schemastore.nvim for jsonls

--- dot_config/nvim/lua/hlong/plugins/lsp.lua
+++ dot_config/nvim/lua/hlong/plugins/lsp.lua
@@ -1,260 +1,3 @@
-local servers = {
-	bashls = {},
-	biome = {},
-	cssls = {},
-	dockerls = {},
-	jdtls = {}, -- nvim-jdtls is not working
-	jsonls = {
-		settings = {
-			json = {
-				schemas = {
-					{
-						description = "TypeScript compiler configuration file",
-						fileMatch = {
-							"tsconfig.json",
-							"tsconfig.*.json",
-						},
-						url = "https://json.schemastore.org/tsconfig.json",
-					},
-					{
-						description = "Lerna config",
-						fileMatch = { "lerna.json" },
-						url = "https://json.schemastore.org/lerna.json",
-					},
-					{
-						description = "Babel configuration",
-						fileMatch = {
-							".babelrc.json",
-							".babelrc",
-							"babel.config.json",
-						},
-						url = "https://json.schemastore.org/babelrc.json",
-					},
-					{
-						description = "ESLint config",
-						fileMatch = {
-							".eslintrc.json",
-							".eslintrc",
-						},
-						url = "https://json.schemastore.org/eslintrc.json",
-					},
-					{
-						description = "Bucklescript config",
-						fileMatch = { "bsconfig.json" },
-						url = "https://raw.githubusercontent.com/rescript-lang/rescript-compiler/8.2.0/docs/docson/build-schema.json",
-					},
-					{
-						description = "Prettier config",
-						fileMatch = {
-							".prettierrc",
-							".prettierrc.json",
-							"prettier.config.json",
-						},
-						url = "https://json.schemastore.org/prettierrc",
-					},
-					{
-						description = "Vercel Now config",
-						fileMatch = { "now.json" },
-						url = "https://json.schemastore.org/now",
-					},
-					{
-						description = "Stylelint config",
-						fileMatch = {
-							".stylelintrc",
-							".stylelintrc.json",
-							"stylelint.config.json",
-						},
-						url = "https://json.schemastore.org/stylelintrc",
-					},
-					{
-						description = "A JSON schema for the ASP.NET LaunchSettings.json files",
-						fileMatch = { "launchsettings.json" },
-						url = "https://json.schemastore.org/launchsettings.json",
-					},
-					{
-						description = "Schema for CMake Presets",
-						fileMatch = {
-							"CMakePresets.json",
-							"CMakeUserPresets.json",
-						},
-						url = "https://raw.githubusercontent.com/Kitware/CMake/master/Help/manual/presets/schema.json",
-					},
-					{
-						description = "Configuration file as an alternative for configuring your repository in the settings page.",
-						fileMatch = {
-							".codeclimate.json",
-						},
-						url = "https://json.schemastore.org/codeclimate.json",
-					},
-					{
-						description = "LLVM compilation database",
-						fileMatch = {
-							"compile_commands.json",
-						},
-						url = "https://json.schemastore.org/compile-commands.json",
-					},
-					{
-						description = "Config file for Command Task Runner",
-						fileMatch = {
-							"commands.json",
-						},
-						url = "https://json.schemastore.org/commands.json",
-					},
-					{
-						description = "AWS CloudFormation provides a common language for you to describe and provision all the infrastructure resources in your cloud environment.",
-						fileMatch = {
-							"*.cf.json",
-							"cloudformation.json",
-						},
-						url = "https://raw.githubusercontent.com/awslabs/goformation/v5.2.9/schema/cloudformation.schema.json",
-					},
-					{
-						description = "The AWS Serverless Application Model (AWS SAM, previously known as Project Flourish) extends AWS CloudFormation to provide a simplified way of defining the Amazon API Gateway APIs, AWS Lambda functions, and Amazon DynamoDB tables needed by your serverless application.",
-						fileMatch = {
-							"serverless.template",
-							"*.sam.json",
-							"sam.json",
-						},
-						url = "https://raw.githubusercontent.com/awslabs/goformation/v5.2.9/schema/sam.schema.json",
-					},
-					{
-						description = "Json schema for properties json file for a GitHub Workflow template",
-						fileMatch = {
-							".github/workflow-templates/**.properties.json",
-						},
-						url = "https://json.schemastore.org/github-workflow-template-properties.json",
-					},
-					{
-						description = "golangci-lint configuration file",
-						fileMatch = {
-							".golangci.toml",
-							".golangci.json",
-						},
-						url = "https://json.schemastore.org/golangci-lint.json",
-					},
-					{
-						description = "JSON schema for the JSON Feed format",
-						fileMatch = {
-							"feed.json",
-						},
-						url = "https://json.schemastore.org/feed.json",
-						versions = {
-							["1"] = "https://json.schemastore.org/feed-1.json",
-							["1.1"] = "https://json.schemastore.org/feed.json",
-						},
-					},
-					{
-						description = "Packer template JSON configuration",
-						fileMatch = {
-							"packer.json",
-						},
-						url = "https://json.schemastore.org/packer.json",
-					},
-					{
-						description = "NPM configuration file",
-						fileMatch = {
-							"package.json",
-						},
-						url = "https://json.schemastore.org/package.json",
-					},
-					{
-						description = "JSON schema for Visual Studio component configuration files",
-						fileMatch = {
-							"*.vsconfig",
-						},
-						url = "https://json.schemastore.org/vsconfig.json",
-					},
-					{
-						description = "Resume json",
-						fileMatch = { "resume.json" },
-						url = "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json",
-					},
-					{
-						description = "Devcontainer",
-						fileMatch = { "devcontainer.json" },
-						url = "https://raw.githubusercontent.com/devcontainers/spec/main/schemas/devContainer.base.schema.json",
-					},
-				},
-			},
-		},
-		setup = {
-			commands = {
-				Format = {
-					function()
-						vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-					end,
-				},
-			},
-		},
-	},
-	lua_ls = {
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = "LuaJIT",
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = { "vim" },
-				},
-				workspace = {
-					-- Make the server aware of Neovim runtime files
-					library = vim.api.nvim_get_runtime_file("", true),
-					checkThirdParty = "Disable",
-				},
-			},
-		},
-	},
-	pyright = {
-		settings = {
-			python = {
-				analysis = {
-					typeCheckingMode = "basic",
-				},
-			},
-		},
-	},
-	ruby_ls = {},
-	ruff_lsp = {},
-	rust_analyzer = {},
-	terraformls = {},
-	tflint = {},
-	tsserver = {
-		init_options = {
-			preferences = { includeCompletionsForModuleExports = false },
-		},
-	},
-	yamlls = {
-		settings = {
-			yaml = {
-				schemas = {
-					kubernetes = {
-						"configmap.yml",
-						"configmap.yaml",
-						"deployment.yml",
-						"deployment.yaml",
-						"hpa.yml",
-						"hpa.yaml",
-						"service.yml",
-						"service.yaml",
-						"serviceaccount.yml",
-						"serviceaccount.yaml",
-					},
-					["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-					["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-					["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-					["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-					["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-					["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-					["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-					["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-				},
-			},
-		},
-	},
-}
-
 local ensure_installed = {
 	"bashls",
 	"cssls",
@@ -394,6 +137,7 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
+		"b0o/schemastore.nvim",
 		"folke/neoconf.nvim",
 		"folke/neodev.nvim",
 		"hrsh7th/cmp-nvim-lsp",
@@ -440,6 +184,76 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
 		capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
+		local servers = {
+			bashls = {},
+			biome = {},
+			cssls = {},
+			dockerls = {},
+			jdtls = {}, -- nvim-jdtls is not working
+			jsonls = {
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+				setup = {
+					commands = {
+						Format = {
+							function()
+								vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+							end,
+						},
+					},
+				},
+			},
+			lua_ls = {
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = { "vim" },
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = "Disable",
+						},
+					},
+				},
+			},
+			pyright = {
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "basic",
+						},
+					},
+				},
+			},
+			ruby_ls = {},
+			ruff_lsp = {},
+			rust_analyzer = {},
+			terraformls = {},
+			tflint = {},
+			tsserver = {
+				init_options = {
+					preferences = { includeCompletionsForModuleExports = false },
+				},
+			},
+			yamlls = {
+				settings = {
+					yaml = {
+						schemastore = { enable = true },
+					},
+				},
+			},
+		}
 
 		for server, opts in pairs(servers) do
 			opts = vim.tbl_deep_extend("force", opts, {
