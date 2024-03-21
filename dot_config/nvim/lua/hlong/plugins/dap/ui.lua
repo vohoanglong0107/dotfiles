3b58a2105a22c2d3d31622c37f37a28e4df8b45e
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-03-21 19:39:38 +0900

    chore: update plugins

--- dot_config/nvim/lua/hlong/plugins/dap/ui.lua
+++ dot_config/nvim/lua/hlong/plugins/dap/ui.lua
@@ -3,6 +3,7 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"mfussenegger/nvim-dap",
+		"nvim-neotest/nvim-nio",
 	},
 	config = function()
 		local dap, dapui = require("dap"), require("dapui")
