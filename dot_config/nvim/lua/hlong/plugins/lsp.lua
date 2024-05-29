4c9f7d055206f7d679d4fa62791103065a83a5f5
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-05-29 22:29:39 +0900

    chore(nvim): remove ruby lsp pull diagnostic setup
    Pull diagnostic is supported on Neovim 0.10.0

--- dot_config/nvim/lua/hlong/plugins/lsp.lua
+++ dot_config/nvim/lua/hlong/plugins/lsp.lua
@@ -37,50 +37,6 @@ end
 
 local lspconfig_group = vim.api.nvim_create_augroup("LspConfig", { clear = true })
 
--- TODO: remove this after neovim support pull diagnostic
--- https://github.com/Shopify/ruby-lsp/blob/main/EDITORS.md#neovim-lsp
-local _timers = {}
-local function setup_ruby_diagnostics(client, buffer)
-	if require("vim.lsp.diagnostic")._enable then
-		return
-	end
-
-	local diagnostic_handler = function()
-		local params = vim.lsp.util.make_text_document_params(buffer)
-		client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
-			if err then
-				local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
-				vim.lsp.log.error(err_msg)
-			end
-			local diagnostic_items = {}
-			if result then
-				diagnostic_items = result.items
-			end
-			vim.lsp.diagnostic.on_publish_diagnostics(
-				nil,
-				vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
-				{ client_id = client.id }
-			)
-		end)
-	end
-
-	diagnostic_handler() -- to request diagnostics on buffer when first attaching
-
-	vim.api.nvim_buf_attach(buffer, false, {
-		on_lines = function()
-			if _timers[buffer] then
-				vim.fn.timer_stop(_timers[buffer])
-			end
-			_timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
-		end,
-		on_detach = function()
-			if _timers[buffer] then
-				vim.fn.timer_stop(_timers[buffer])
-			end
-		end,
-	})
-end
-
 local function setup_auto_format_on_save(client, bufnr)
 	if client.server_capabilities.documentFormattingProvider then
 		vim.api.nvim_create_autocmd("BufWritePre", {
@@ -122,10 +78,6 @@ local on_attach = function(client, bufnr)
 	end
 	if client.name == "yamlls" then
 		disable_yamlls_on_helm(client, bufnr)
-	end
-
-	if client.name == "ruby_lsp" then
-		setup_ruby_diagnostics(client, bufnr)
 	end
 
 	lsp_keymaps(bufnr)
