d492ef924b637a9fd327bb7f222a1c3b397596de
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-05-31 00:38:41 +0900

    feat(nvim): keymaps for neotest

--- dot_config/nvim/lua/hlong/plugins/neotest.lua
+++ dot_config/nvim/lua/hlong/plugins/neotest.lua
@@ -7,6 +7,36 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "mrcjkb/rustaceanvim",
   },
+  keys = {
+    {
+      "<leader>tr",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run closest test",
+    },
+    {
+      "<leader>to",
+      function()
+        require("neotest").output.open({ enter = true })
+      end,
+      desc = "Open test output",
+    },
+    {
+      "[t",
+      function()
+        require("neotest").jump.prev({ status = "failed" })
+      end,
+      desc = "Jump to previous failed test",
+    },
+    {
+      "]t",
+      function()
+        require("neotest").jump.next({ status = "failed" })
+      end,
+      desc = "Jump to next failed test",
+    },
+  },
   config = function()
     require("neotest").setup({
       adapters = {
