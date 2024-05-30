ea3d7430a3ec92067665702adecffebf9600b43c
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-05-30 11:16:56 +0900

    chore(nvim): remove format setup for jsonls
    One should use prettier or Biome to format json
    I'm not sure if this option has any effect

--- dot_config/nvim/lua/hlong/plugins/lsp.lua
+++ dot_config/nvim/lua/hlong/plugins/lsp.lua
@@ -150,15 +150,6 @@ return {
 						validate = { enable = true },
 					},
 				},
-				setup = {
-					commands = {
-						Format = {
-							function()
-								vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-							end,
-						},
-					},
-				},
 			},
 			lua_ls = {
 				settings = {
