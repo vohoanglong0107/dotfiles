10b245f1e54f8e2d159dd8945562679527193a3f
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-05-31 01:08:55 +0900

    feat(nvim): use catpuccin integration

--- dot_config/nvim/lua/hlong/plugins/catppuccin.lua
+++ dot_config/nvim/lua/hlong/plugins/catppuccin.lua
@@ -1,46 +1,51 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = true,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = true,
-			integrations = {
-				leap = true,
-				notify = true,
-				native_lsp = {
-					enabled = true,
-					virtual_text = {
-						errors = { "italic" },
-						hints = { "italic" },
-						warnings = { "italic" },
-						information = { "italic" },
-					},
-					underlines = {
-						errors = { "underline" },
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-				},
-			},
-			highlight_overrides = {
-				all = function(colors)
-					return {
-						LspInfoBorder = { fg = colors.text },
-						IndentBlanklineSpaceCharBlankline = { fg = colors.text },
-						IndentBlanklineSpaceChar = { fg = colors.text },
-						DashboardProjectTitle = { fg = colors.sapphire },
-						DashboardProjectTitleIcon = { fg = colors.sapphire },
-						DashboardProjectIcon = { fg = colors.sapphire },
-						DashboardMruTitle = { fg = colors.peach },
-						DashboardMruIcon = { fg = colors.peach },
-						DashboardFiles = { fg = colors.text },
-						DashboardShotCutIcon = { fg = colors.peach },
-					}
-				end,
-			},
-		})
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = true,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        dashboard = true,
+        gitsigns = true,
+        illuminate = {
+          enabled = true,
+          lsp = false,
+        },
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = false,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = { background = true },
+        },
+        neotest = true,
+        notify = true,
+        telescope = { enabled = true },
+        treesitter = true,
+        which_key = true,
+      },
+      highlight_overrides = {
+        all = function(colors)
+          return {
+            LspInfoBorder = { fg = colors.text },
+          }
+        end,
+      },
+    })
+  end,
 }
