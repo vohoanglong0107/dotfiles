bb56abc0de016c152b78595e2ded8e616e59cde3
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-05-31 10:16:06 +0900

    feat(nvim): add run tests keymap

--- dot_config/nvim/lua/hlong/plugins/neotest.lua
+++ dot_config/nvim/lua/hlong/plugins/neotest.lua
@@ -1,47 +1,61 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "mrcjkb/rustaceanvim",
-  },
-  keys = {
-    {
-      "<leader>tr",
-      function()
-        require("neotest").run.run()
-      end,
-      desc = "Run closest test",
-    },
-    {
-      "<leader>to",
-      function()
-        require("neotest").output.open({ enter = true })
-      end,
-      desc = "Open test output",
-    },
-    {
-      "[t",
-      function()
-        require("neotest").jump.prev({ status = "failed" })
-      end,
-      desc = "Jump to previous failed test",
-    },
-    {
-      "]t",
-      function()
-        require("neotest").jump.next({ status = "failed" })
-      end,
-      desc = "Jump to next failed test",
-    },
-  },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("rustaceanvim.neotest"),
-      },
-    })
-  end,
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"mrcjkb/rustaceanvim",
+	},
+	keys = {
+		{
+			"<leader>tr",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "Run closest test",
+		},
+		{
+			"<leader>tf",
+			function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end,
+			desc = "Run all tests in current buffer",
+		},
+		{
+			"<leader>ta",
+			function()
+				require("neotest").run.run({ suite = true })
+			end,
+			desc = "Run all tests",
+		},
+		{
+			"<leader>to",
+			function()
+				require("neotest").output.open({ enter = true })
+			end,
+			desc = "Open test output",
+		},
+		{
+			"[t",
+			function()
+				require("neotest").jump.prev({ status = "failed" })
+			end,
+			desc = "Jump to previous failed test",
+		},
+		{
+			"]t",
+			function()
+				require("neotest").jump.next({ status = "failed" })
+			end,
+			desc = "Jump to next failed test",
+		},
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("rustaceanvim.neotest"),
+			},
+		})
+	end,
 }
