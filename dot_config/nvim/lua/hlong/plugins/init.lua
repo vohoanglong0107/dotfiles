05274e5353c13254056ea19cef44d4ac8ac72e45
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-07-12 17:23:31 +0900

    refactor(nvim): move simple plugin configs to central place
    Listing installed plugins as files is essentially the same as listing
    them inside a single central file if that plugin need little to no
    configuration, so we should leave separate config file for those plugin
    that really need it, and leave the plugins folder unclustered

--- dot_config/nvim/lua/hlong/plugins/init.lua
+++ dot_config/nvim/lua/hlong/plugins/init.lua
@@ -1,6 +1,18 @@
 return {
-  { "folke/lazy.nvim" },
-  { "towolf/vim-helm" },
-  { "github/copilot.vim" },
-  { "RRethy/vim-illuminate" },
+	{ "folke/lazy.nvim" },
+	{ "github/copilot.vim" },
+	{ "nvim-pack/nvim-spectre", lazy = true, opts = {} },
+	{ "moll/vim-bbye", cmd = "Bdelete" },
+	-- Language specific
+	{ "towolf/vim-helm" },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+	-- UI
+	{ "folke/trouble.nvim", cmd = { "Trouble" }, opts = {} },
+	{ "RRethy/vim-illuminate" },
+	{ "HiPhish/rainbow-delimiters.nvim" },
+	{ "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" }, opts = {} },
 }
