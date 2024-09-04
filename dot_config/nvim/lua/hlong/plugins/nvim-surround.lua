4282f130c9419f9a05c7fb3f0546e86d1d5cfa5c
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-09-04 11:27:26 +0900

    refactor(nvim): group text manipulation plugins

--- dot_config/nvim/lua/hlong/plugins/nvim-surround.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"kylechui/nvim-surround",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-treesitter/nvim-treesitter-textobjects" },
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		require("nvim-surround").setup({})
-	end,
-}
