ba45817addc75948e734f8ea0c9dbe299a2a1c1a
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-11-13 22:41:52 +0900

    feat(nvim): disable type hints when inlayHints is on
    It causes a lot of noise. Whenever I need to view the type of any
    variable we can just hover over it instead

--- dot_config/nvim/lua/hlong/languages/rust.lua
+++ dot_config/nvim/lua/hlong/languages/rust.lua
@@ -1,10 +1,21 @@
 vim.g.rustaceanvim = {
-  tools = {
-    hover_actions = {
-      replace_builtin_hover = false,
-    },
-    float_win_config = {
-      border = "rounded",
-    },
-  },
+	server = {
+		default_settings = {
+			["rust-analyzer"] = {
+				inlayHints = {
+					typeHints = {
+						enable = false,
+					},
+				},
+			},
+		},
+	},
+	tools = {
+		hover_actions = {
+			replace_builtin_hover = false,
+		},
+		float_win_config = {
+			border = "rounded",
+		},
+	},
 }
