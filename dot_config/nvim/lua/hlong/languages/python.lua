7e115b90a0bc4a76db41338cd81695b24eae1ca0
vohoanglong0107 <vo.hoang.long@outlook.com>
2025-02-18 18:48:02 +0900

    fix(nvim): remove black as a formatter
    We have ruff

--- dot_config/nvim/lua/hlong/languages/python.lua
+++ dot_config/nvim/lua/hlong/languages/python.lua
@@ -1,26 +1,20 @@
 local base = require("hlong.languages.base")
 local lspconfig = require("lspconfig")
-local null_ls = require("null-ls")
 
 lspconfig.pyright.setup({
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "basic",
-      },
-    },
-  },
-  capabilities = base.capabilities,
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "basic",
+			},
+		},
+	},
+	capabilities = base.capabilities,
 })
 
 lspconfig.ruff_lsp.setup({
-  on_attach = function(client, _)
-    client.server_capabilities.hoverProvider = false
-  end,
-  capabilities = base.capabilities,
+	on_attach = function(client, _)
+		client.server_capabilities.hoverProvider = false
+	end,
+	capabilities = base.capabilities,
 })
-
--- TODO: we have ruff_lsp, so we don't need this
-null_ls.register(null_ls.builtins.formatting.black.with({
-  extra_args = { "--fast" },
-}))
