70429a5acca2e9a6688395ded9e9b343b065610a
vohoanglong0107 <vo.hoang.long@outlook.com>
2025-02-18 18:45:43 +0900

    fix: find atuin installed using the official script

--- dot_zsh/tools/atuin.zsh
+++ dot_zsh/tools/atuin.zsh
@@ -1,1 +1,10 @@
-eval "$(atuin init zsh)"
+if command -v atuin &> /dev/null; then
+  # atuin is installed package manager
+  ATUIN_BIN=atuin
+else
+  # atuin is installed with official script: https://atuin.sh/
+  export PATH=~/.atuin/bin:$PATH
+  ATUIN_BIN=~/.atuin/bin/atuin
+fi
+
+eval "$($ATUIN_BIN init zsh)"
