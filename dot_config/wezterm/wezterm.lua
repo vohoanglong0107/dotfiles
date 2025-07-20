7137f36c17dd8b249da8a89358743db68350683b
vohoanglong0107 <vo.hoang.long@outlook.com>
2025-07-20 16:42:37 +0900

    fix(wezterm): only apply toolbox and container domains on linux

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,13 +1,17 @@
 local wezterm = require("wezterm")
-local container_domain = require("container_domain")
-local toolbox_domain = require("toolbox_domain")
 local catppuccin = wezterm.plugin.require("https://github.com/catppuccin/wezterm")
 
 local config = wezterm.config_builder()
 
-container_domain.apply_to_config(config)
-toolbox_domain.apply_to_config(config)
 catppuccin.apply_to_config(config, { sync = true })
+
+if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+	local container_domain = require("container_domain")
+	-- FIXME: should only apply toolbox on Fedora machines
+	local toolbox_domain = require("toolbox_domain")
+	container_domain.apply_to_config(config)
+	toolbox_domain.apply_to_config(config)
+end
 
 config.window_background_opacity = 0.85
 config.set_environment_variables = {
