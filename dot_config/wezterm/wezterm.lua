ed0b4c97f90d793fadaabcab8d2549f999690f59
vohoanglong0107 <vo.hoang.long@outlook.com>
2025-08-23 02:46:16 +0900

    feat: styling wezterm title bar

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local catppuccin = wezterm.plugin.require("https://github.com/catppuccin/wezterm")
+local helpers = require("helpers")
 
 local config = wezterm.config_builder()
 
@@ -14,12 +15,16 @@ if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
 end
 
 config.window_background_opacity = 0.85
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+
 config.set_environment_variables = {
 	EDITOR = "nvim",
 	PATH = "/opt/homebrew/bin:/usr/local/bin:" .. os.getenv("PATH"),
 }
 config.font = wezterm.font("JetBrains Mono")
-config.enable_tab_bar = false
 
 config.keys = {
 	{ key = "UpArrow", mods = "SHIFT", action = wezterm.action.ScrollByLine(-1) },
@@ -33,6 +38,40 @@ end
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "pwsh" }
 end
+
+local mocha_palette = {
+	crust = "#11111b",
+	base = "#1e1e2e",
+	text = "#cdd6f4",
+	blue = "#89b4fa",
+	mantle = "#181825",
+	surface0 = "#313244",
+}
+
+-- For some reason these settings are not applied by catppuccin plugin
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			bg_color = mocha_palette.blue,
+			fg_color = mocha_palette.crust,
+		},
+		inactive_tab = {
+			bg_color = mocha_palette.mantle,
+			fg_color = mocha_palette.text,
+		},
+		inactive_tab_hover = {
+			bg_color = mocha_palette.base,
+			fg_color = mocha_palette.text,
+		},
+
+		inactive_tab_edge = mocha_palette.surface0,
+	},
+}
+
+config.window_frame = {
+	font = wezterm.font("JetBrains Mono"),
+	-- font_size = 10,
+}
 
 -- config.exit_behavior = "Hold"
 
